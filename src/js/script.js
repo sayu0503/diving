7e6a0c48d252e61a0e59ecb95df2168083451219
sayu <sayu36490@gmail.com>
2024-11-01 15:30:34 +0900

    1101

--- src/js/script.js
+++ src/js/script.js
@@ -188,3 +188,11 @@ $(document).ready(function() {
       tabContent.eq(index).addClass("is-active");
     });
   });
+
+  //サイドバーアコーディオン
+  $(function () {
+    $(".js-accordion-title").on("click", function () {
+      $(this).next().slideToggle(300);
+      $(this).toggleClass("open", 300);
+    });
+  });
