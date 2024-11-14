f804beaa60e29104862c8a6757cfc91a7dd326c7
sayu <sayu36490@gmail.com>
2024-11-14 15:43:23 +0900

    1114

--- src/js/script.js
+++ src/js/script.js
@@ -67,7 +67,7 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
     speed: 1500,
     autoplay: {
       delay: 1000,
-      disableOnInteraction: false,//ユーザーがスライドを操作しても、自動再生が止まらない
+      disableOnInteraction: false,
     },
     breakpoints: {
       // 768px以上の場合
@@ -97,10 +97,9 @@ $(function () {
     } else {
       $(".js-page-top").fadeOut();
     }
-
-    const scrollHeight = $(document).height();/*ページ全体の高さ*/
-    const scrollPosition = $(window).height() + $(window).scrollTop();/*ページの一番上からスクロールされた距離*/
-    const footHeight = $('footer').outerHeight();/*フッターの高さ*/
+    const scrollHeight = $(document).height();
+    const scrollPosition = $(window).height() + $(window).scrollTop();
+    const footHeight = $('footer').outerHeight();
 
     if (scrollHeight - scrollPosition <= footHeight) {
       $(".js-page-top").css({
@@ -117,20 +116,15 @@ $(function () {
 });
 
 //背景色の後に画像やテキストが表示されるエフェクト
-//要素の取得とスピードの設定
 var box = $('.colorbox'),
   speed = 700;
-
-//.colorboxの付いた全ての要素に対して下記の処理を行う
 box.each(function () {
   $(this).append('<div class="color"></div>')
   var color = $(this).find($('.color')),
     image = $(this).find('img');
   var counter = 0;
-
   image.css('opacity', '0');
   color.css('width', '0%');
-  //inviewを使って背景色が画面に現れたら処理をする
   color.on('inview', function () {
     if (counter == 0) {
       $(this).delay(200).animate({ 'width': '100%' }, speed, function () {
@@ -145,19 +139,14 @@ box.each(function () {
 
 //モーダルウィンドウ
 $(document).ready(function () {
-  // 画像がクリックされたときの処理
   $('.js-modal-open').on('click', function (e) {
-    e.preventDefault(); // リンクのデフォルト動作を無効化
-
-    const imgSrc = $(this).data('image'); // data-imageの値を取得
-    $('.modal__image').attr('src', imgSrc); // モーダル内の画像にsrcを設定
-    const imgIndex = $(this).data('index'); // data-index属性から画像番号を取得
-
-    $('.js-modal').fadeIn(); // モーダルを表示
-    $('.modal__image').css('opacity', '1'); // 画像の透明度を1に設定
-    // スクロールを無効にするために body にクラスを追加
+    e.preventDefault();
+    const imgSrc = $(this).data('image');
+    $('.modal__image').attr('src', imgSrc);
+    const imgIndex = $(this).data('index');
+    $('.js-modal').fadeIn();
+    $('.modal__image').css('opacity', '1');
     $('body').addClass('no-scroll');
-    // 画像が1枚目または6枚目なら50vwに設定、それ以外は100vw
     if (imgIndex === 1 || imgIndex === 6) {
       $('.modal__content').css('width', '50vw');
     } else {
@@ -166,20 +155,31 @@ $(document).ready(function () {
   });
 });
 
-// モーダルを閉じるときの処理
 $('.js-modal-close').on('click', function () {
   $('.js-modal').fadeOut(function () {
     $('.modal__image').css('opacity', '0');
   });
-
-  // スクロールを有効にするためにクラスを削除
   $('body').removeClass('no-scroll');
 });
 
 //タブ
 $(function () {
   const tabButton = $(".js-tab-button"),
-    tabContent = $(".js-tab-content");
+        tabContent = $(".js-tab-content");
+
+  const hash = window.location.hash;
+  if (hash) {
+    const targetTab = $(hash);
+    if (targetTab.length) {
+
+      tabButton.removeClass("is-active");
+      tabContent.removeClass("is-active");
+
+      const index = tabContent.index(targetTab);
+      tabButton.eq(index).addClass("is-active");
+      targetTab.addClass("is-active");
+    }
+  }
   tabButton.on("click", function () {
     let index = tabButton.index(this);
     tabButton.removeClass("is-active");
@@ -189,7 +189,8 @@ $(function () {
   });
 });
 
-//サイドバーアのコーディオン
+
+//サイドバーのコーディオン
 $(function () {
   $(".js-accordion-title").on("click", function () {
     $(this).next().slideToggle(300);
@@ -199,61 +200,45 @@ $(function () {
 
 //FAQのアコーディオン
 $(document).ready(function () {
-  // 初期状態でcurrentクラスが付いている項目を開いた状態にする
   $('.accordion__content.current').each(function () {
-    $(this).show(); // 内容を表示
-    $(this).prev('.accordion__header').addClass('is-open'); // 見出しにis-openクラスを追加
+    $(this).show();
+    $(this).prev('.accordion__header').addClass('is-open');
   });
-  // タイトルをクリックした場合
   $('.accordion__header').click(function () {
-    // アコーディオン（内容）の開閉
     $(this).next('.accordion__content').slideToggle(200);
-    // タイトルにopenクラスを付与または削除してプラスマイナス可変
     $(this).toggleClass("is-open", 200);
   });
 });
 
 //コンタクトフォームのバリデーション
-$(document).ready(function() {
-  $(".page-contact__button").on("click", function(event) {
-      // エラーメッセージボックスを一旦非表示
-      $(".page-contact__error-box").hide();
-      // フォームが有効かどうかを示すフラグ
-      let isValid = true;
-      // 必須項目をチェックし、未入力の項目にエラースタイルを適用
-      $(".js-form").find("input[required], textarea[required], select[required]").each(function() {
-          if ($(this).val().trim() === "") {
-              // 未入力の場合、`use-invalid`クラスを追加してエラースタイルを適用
-              $(this).addClass("use-invalid");
-              isValid = false;
-          } else {
-              // 入力されている場合は`use-invalid`クラスを削除して正常スタイルに戻す
-              $(this).removeClass("use-invalid");
-          }
-      });
-        // チェックボックスの必須項目バリデーション
-        if ($(".page-contact__checkbox-group input[type='checkbox']:checked").length === 0) {
-          $(".page-contact__checkbox-group input[type='checkbox']").addClass("use-invalid");
-          $(".page-contact__checkbox-error").show();
-          isValid = false;
+$(document).ready(function () {
+  $(".page-contact__button").on("click", function (event) {
+    $(".page-contact__error-box").hide();
+    let isValid = true;
+    $(".js-form").find("input[required], textarea[required], select[required]").each(function () {
+      if ($(this).val().trim() === "") {
+        $(this).addClass("use-invalid");
+        isValid = false;
+      } else {
+        $(this).removeClass("use-invalid");
       }
-
-      // フォームが無効な場合、エラーメッセージボックスを表示し、ページを最上部に移動
-      if (!isValid) {
-          $(".page-contact__error-box").show();
-          // ページを最上部に即座に移動
-          window.scrollTo(0, 0);
-          // フォーム送信をキャンセル
-          return false;
-      }
-       // フォームが有効な場合、page-thanks.htmlに遷移
-       window.location.href = "page-thanks.html";
-  });
-  // チェックボックスの状態を監視し、1つでもチェックが入ればエラースタイルを解除
-  $(".page-contact__checkbox-group input[type='checkbox']").on("change", function() {
-    if ($(".page-contact__checkbox-group input[type='checkbox']:checked").length > 0) {
-        $(".page-contact__checkbox-group input[type='checkbox']").removeClass("use-invalid");
+    });
+    if ($(".page-contact__checkbox-group input[type='checkbox']:checked").length === 0) {
+      $(".page-contact__checkbox-group input[type='checkbox']").addClass("use-invalid");
+      $(".page-contact__checkbox-error").show();
+      isValid = false;
     }
-});
+    if (!isValid) {
+      $(".page-contact__error-box").show();
+      window.scrollTo(0, 0);
+      return false;
+    }
+    window.location.href = "page-thanks.html";
+  });
+  $(".page-contact__checkbox-group input[type='checkbox']").on("change", function () {
+    if ($(".page-contact__checkbox-group input[type='checkbox']:checked").length > 0) {
+      $(".page-contact__checkbox-group input[type='checkbox']").removeClass("use-invalid");
+    }
+  });
 });
 
